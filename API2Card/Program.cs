8f68026890ff2005fcb4f721a1545628fcf4b00c
masshuku <supermatthew2@gmail.com>
2020-01-16 02:57:57 -0600

    Update Program.cs

--- API2Card/Program.cs
+++ API2Card/Program.cs
@@ -1,4 +1,4 @@
-﻿using API2Card.JSON.Spells;
+using API2Card.JSON.Spells;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
@@ -12,36 +12,58 @@ namespace API2Card
         static void Main(string[] args)
         {
 
-#if DEBUG
-            LoadJson("https://api.open5e.com/spells/?level_int__iexact=&limit=500", "./test.json");
-#else
-            if(args.Length != 2) {
-                Console.WriteLine("Requires 2 arguments, the API url to parse and an output file name. Wrap these in double quotes if you have any issues.");
+            //LoadJson("S25x35","https://api.open5e.com/spells/?level_int__iexact=", "./test.json");
+            if (args.Length != 3) {
+                PrintHelp();
                 return;
             }
-            LoadJson(args[0], args[1]);
-#endif
+            LoadJson(args[0], args[1], args[2]);
         }
 
-        public static void LoadJson(string uri, string file)
+        private static void PrintHelp()
         {
+            Console.WriteLine("Requires 3 arguments, the card size, the API url to parse, and an output file name. Wrap these in double quotes to prevent commabd line issues.");
+            Console.WriteLine("Current card sizes supported: ");
+            foreach (CardSize suit in (CardSize[])Enum.GetValues(typeof(CardSize)))
+            {
+                Console.WriteLine(suit.ToString());
+            }
+        }
+
+        public static void LoadJson(string cardSizeArg, string uri, string file)
+        {
+            CardSize cardSize;
+            if(!Enum.TryParse<CardSize>(cardSizeArg, out cardSize))
+            {
+                Console.WriteLine("Unknown card size {0}", cardSizeArg);
+                PrintHelp();
+                return;
+            }
+
+            bool Next = true;
+            List<SpellCard> CardList = new List<SpellCard>();
             using (WebClient wc = new WebClient())
             {
+                while (Next)
+                {
+                    string json = wc.DownloadString(uri);
+                    Result SearchResults = JsonConvert.DeserializeObject<Result>(json);
 
-                string json = wc.DownloadString(uri);
-                Result SearchResults = JsonConvert.DeserializeObject<Result>(json);
-                List<SpellCard> CardList = new List<SpellCard>();
-                foreach (Spell s in SearchResults.results) {
-                    //24 lines, 55 characters per line
-                    CardSizeData csd = new CardSizeData();
-                    csd.Size = CardSize.S75x50;
-                    SpellCard[] Cards = SpellCard.GenerateSpellCard(s, CardType.Spell, csd);
-                    CardList.AddRange(Cards);
+                    foreach (Spell s in SearchResults.results)
+                    {
+                        CardSizeData csd = new CardSizeData();
+                        csd.Size = cardSize;
+                        SpellCard[] Cards = SpellCard.GenerateSpellCard(s, CardType.Spell, csd);
+                        CardList.AddRange(Cards);
+                    }
+
+                    if (SearchResults.next != null)
+                        uri = SearchResults.next;
+                    else
+                        Next = false;
                 }
-                File.WriteAllText(file, JsonConvert.SerializeObject(CardList.ToArray()));
-                //Console.WriteLine(JsonConvert.SerializeObject(CardList.ToArray()));
-                //Console.ReadKey();
             }
+            File.WriteAllText(file, JsonConvert.SerializeObject(CardList.ToArray()));
         }
     }
 }
